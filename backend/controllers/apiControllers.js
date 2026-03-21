cb6defd770f3e42562301e1ce6bb1ce806352180
neg1-git <sahilnegi.bt23mech@pec.edu.in>
2026-03-22 01:55:15 +0530

    finished CRUD basic

--- backend/controllers/apiControllers.js
+++ backend/controllers/apiControllers.js
@@ -14,6 +14,41 @@ const addItems = async(req,res)=>{
   }
 }
 
+const getItems= async(req,res)=>{
+  let user_id=req.user
+  try {
+    const result=await db.query('Select * from clothing_items where user_id=$1 ORDER BY created_at DESC',[user_id])
+    return res.status(200).json({success:true,data:result.rows})
+  } catch (error) {
+    return res.status(500).json({success:false,msg:'SERVER ERROR'})
+  }
+}
+
+const deleteItem=async(req,res)=>{
+  const {id}=req.params;
+  const userId=req.user;
+  try {
+    const check= await db.query('select user_id from clothing_items where id=$1',[id])
+
+    if(!check.rows[0]){
+      return res.status(404).json({success:false,msg:'ITEM DOESNT EXIST'})
+    }
+    
+    if(userId!==check.rows[0].user_id){
+      return res.status(403).json({success:false,msg:'NOT AUTHORISED!!!'})
+    }else{
+      const result=await db.query('delete from clothing_items where id=$1 returning *',[id])
+
+      return res.status(200).json({success:true,data:result.rows[0]})
+    }
+    
+  } catch (error) {
+    return res.status(500).json({success:false,msg:'SERVER ERROR'})
+  }
+}
+
 module.exports={
-  addItems
+  addItems,
+  getItems,
+  deleteItem
 }
--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -2,9 +2,13 @@ const express=require('express')
 const router=express.Router()
 const {Authorize}=require('../middlewares/authorisation')
 const { 
-  addItems
+  addItems,
+  getItems,
+  deleteItem
 } = require('../controllers/apiControllers')
 
 router.route('/clothes').post(Authorize,addItems)
+router.route('/wardrobe').get(Authorize,getItems)
+router.route('/wardrobe/:id').delete(Authorize,deleteItem)
 
 module.exports=router
