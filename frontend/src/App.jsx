16c6a7b7cddebc8afbf270069b69af9212c3fb28
neg1-git <sahilnegi.bt23mech@pec.edu.in>
2026-03-24 03:49:55 +0530

    set up react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex'>
+      <h1 className='p-2 bg-amber-400'>MAKE LOG IN YOURSELF</h1>
+    </div>
+  )
+}
+
+export default App
